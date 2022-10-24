8d17d512935d53c61e1cd1854a4cd67a676e3d23
Benn Beckman <anselmo27hh@gmail.com>
2022-10-24 02:48:07 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+      id: 0,
+      title: "Mi primer tarea",
+      description: "mi primer tarera",
+    },
+    {
+      id: 1,
+      title: "Mi segunda tarea",
+      description: "mi segunda tarera",
+    },
+    {
+      id: 2,
+      title: "Mi tercera tarea",
+      description: "mi tercera tarera",
+    },
+  ];
